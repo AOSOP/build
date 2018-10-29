e5bced4f6e95b19060041b4621c40fce26f8c1ef
Subinsmani <subins.mani@gmail.com>
2018-10-29 12:30:41 +0530

    build: Pixys Versioning
    Change-Id: I20027345d9f82bc8e27aa3ee5dc2af8c7cdb9553

--- core/main.mk
+++ core/main.mk
@@ -162,6 +162,10 @@ else
   endif
 endif
 
+#
+# -----------------------------------------------------------------
+# Add PixysOS generic properties to the build properties.
+ADDITIONAL_BUILD_PROPERTIES += $(PRODUCT_GENERIC_PROPERTIES)
 
 # Bring in standard build system definitions.
 include $(BUILD_SYSTEM)/definitions.mk
@@ -360,7 +364,7 @@ ADDITIONAL_BUILD_PROPERTIES += dalvik.vm.stack-trace-dir=/data/anr
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/pixys/build/core/main.mk
+#-include vendor/pixys/config/pixys_version.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
